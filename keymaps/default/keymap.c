3518b18ef6b9837906950e7e09311ee487041eb5
coincase1 <kyle.sleggs@gmail.com>
2026-06-09 09:00:02 -0400

    Add files via upload

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,24 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐ ┌───┐
+     * │TAB| Q │ W │ E │ R │ T │ Y │ U | I │ O │ P | - │Bks│PUp| |Knb|
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┴───┼───┤ └───┘
+     * │CPS│ A │ S │ D │ F │ G │ H │ J | K │ L │ ' | ENTER │PDn│ ┌───┐
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───────┼───┤ |Pnt|
+     * │SHF│ Z │ X │ C │ V │ B │ N │ M | , │ . │ / | SHIFT │Up │ └───┘
+     * ├───┼───┼───┼───┴───┴──┬┴───┴───┴──┬┴───┴───┼───┬───┼───┼───┐
+     * │CTR│WIN│ALT│ SPACEBAR │    M(1)   │  M(2)  │ALT│Lft|Dn |Rgt|
+     * └───┴───┴───┴──────────┴───────────┴────────┴───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_TAB,         KC_Q,           KC_W,           KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,           KC_I,       KC_O,       KC_P,       KC_MINUS,       KC_BACKSPACE,   KC_PGUP,    KC_KB_MUTE,
+        KC_CAPS,        KC_A,           KC_S,           KC_D,       KC_F,       KC_G,       KC_H,       KC_J,           KC_K,       KC_L,       KC_QUOTE,   KC_ENTER,       KC_PGDN,        KC_PSCR,
+        KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,       KC_V,       KC_B,       KC_N,       KC_M,           KC_COMMA,   KC_DOT,     KC_SLASH,   KC_RIGHT_SHIFT, KC_UP,
+        KC_LEFT_CTRL,   KC_LEFT_GUI,    KC_LEFT_ALT,    KC_SPACE,   KC_SPACE,   KC_SPACE,   KC_SPACE,   KC_RIGHT_ALT,   KC_LEFT,    KC_DOWN,    KC_RIGHT
+    ),
+};
---
+++ keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x93, 0x88, 0xED, 0xED, 0x0F, 0xC6, 0xBF, 0xF6}
+#define VIAL_UNLOCK_COMBO_ROWS { 0 , 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 2 , 1 }
